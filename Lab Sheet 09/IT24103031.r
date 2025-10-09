d14dc0eaec33c8357b6166de40332349e967fd95
Denuwan-lab <IT24103031@my.sliit.lk>
2025-10-09 22:58:18 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT24103031.r
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\DENUWAN\\Desktop\\IT24103031")
+#Exercise
+# i.
+bake <- rnorm(25, mean = 45, sd = 2)
+bake
+
+#ii. 
+result <- t.test(bake, mu = 46, alternative = "less")
+result
+
+# Extract specific values
+result$statistic  
+result$p.value     
+result$conf.int    
+
